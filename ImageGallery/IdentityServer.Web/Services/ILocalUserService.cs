acc5b01cb6bb608551e612af87d59a4f12587370
u-ashikov <u.ashikov@abv.bg>
2023-11-19 23:31:15 +0200

    Created a service to interact with the users (WIP)

---
+++ ImageGallery/IdentityServer.Web/Services/ILocalUserService.cs
@@ -0,0 +1,20 @@
+﻿namespace IdentityServer.Web.Services;
+
+using IdentityServer.Web.Data.Models;
+
+public interface ILocalUserService
+{
+    Task<bool> ValidateCredentialsAsync(string userName, string password);
+
+    Task<IEnumerable<UserClaim>> GetUserClaimsBySubjectAsync(string subject);
+
+    Task<User> GetUserByUserNameAsync(string userName);
+
+    Task<User> GetUserBySubjectAsync(string subject);
+
+    void AddUser(User userToAdd);
+
+    Task<bool> IsUserActive(string subject);
+
+    Task<bool> SaveChangesAsync();
+}
---
+++ ImageGallery/IdentityServer.Web/Services/LocalUserService.cs
@@ -0,0 +1,41 @@
+﻿namespace IdentityServer.Web.Services;
+
+using IdentityServer.Web.Data.Models;
+
+public class LocalUserService : ILocalUserService
+{
+    public Task<bool> ValidateCredentialsAsync(string userName, string password)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<UserClaim>> GetUserClaimsBySubjectAsync(string subject)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> GetUserByUserNameAsync(string userName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> GetUserBySubjectAsync(string subject)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddUser(User userToAdd)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> IsUserActive(string subject)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> SaveChangesAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
