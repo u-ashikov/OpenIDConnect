61f86851792df3d3505bf07d6ff96a902e089b98
u-ashikov <u.ashikov@abv.bg>
2023-12-18 22:57:40 +0200

    Created a mapper for mapping the claims comming from the external provider to Jwt ones

---
+++ ImageGallery/IdentityServer.Web/Helpers/ExternalIdentityProviderClaimMapper.cs
@@ -0,0 +1,48 @@
+﻿namespace IdentityServer.Web.Helpers;
+
+using System.Security.Claims;
+using IdentityModel;
+
+public class ExternalIdentityProviderClaimMapper
+{
+    private readonly IDictionary<string, Func<string, string>> _externalIdentityProviderClaimMappings = new Dictionary<string, Func<string, string>>();
+
+    public ExternalIdentityProviderClaimMapper()
+    {
+        this._externalIdentityProviderClaimMappings.Add("Facebook", this.MapFacebookClaim);
+    }
+
+    public ICollection<Claim> MapClaims(string authenticationScheme, IEnumerable<Claim> inputClaims)
+    {
+        if (inputClaims is null)
+            return null;
+
+        if (!this._externalIdentityProviderClaimMappings.ContainsKey(authenticationScheme))
+            return null;
+
+        var claimMapper = this._externalIdentityProviderClaimMappings[authenticationScheme];
+        var iteratedInputClaims = inputClaims.ToArray();
+        var outputClaims = new List<Claim>(capacity: iteratedInputClaims.Length);
+
+        foreach (var claim in iteratedInputClaims)
+        {
+            var outputClaimType = claimMapper(claim.Type);
+            if (string.IsNullOrWhiteSpace(outputClaimType)) continue;
+            
+            outputClaims.Add(new Claim(outputClaimType, claim.Value));
+        }
+
+        return outputClaims;
+    }
+
+    private string MapFacebookClaim(string inputClaim)
+    {
+        return inputClaim switch
+        {
+            "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname" => JwtClaimTypes.GivenName,
+            "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname" => JwtClaimTypes.FamilyName,
+            "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress" => JwtClaimTypes.Email,
+            _ => string.Empty
+        };
+    }
+}
